e0eea8f3fe25e55d8d863a6f8284850d00304647
SeanieC <turnip1.0@gmail.com>
2017-12-17 18:02:07 +0000

    final

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,11 +1,63 @@
 package main.java;
 
-public class Ball {
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.logging.Logger;
 
-    public static int maxBall(int v0) {
+import static java.math.BigDecimal.*;
+
+public class Ball {
+    private static final Logger log = Logger.getLogger("Ball.class");
+
+
+    public static int maxBall(int intInitSpeed) {
         // your code
-        int answer = 0;
-        return answer;
+        int maxHeight = 0;
+        //    log.info("Starting...");
+        // if ( intInitSpeed > 0) {
+        maxHeight = calculatemaxHeight(intInitSpeed);
+        // }
+        //   log.info("Completing...");
+
+        return maxHeight;
     }
+
+    private static int calculatemaxHeight(int intInitSpeed) {
+        double time = ((intInitSpeed * 1000) / 3600) / 9.81;
+        log.info("TIME: " + time);
+
+//        if (== new BigDecimal(time).setScale(1, RoundingMode.CEILING).doubleValue()) {
+//            time = (int) (Math.ceil(time) * 10);
+//        } else {
+//            time = (int) (Math.floor(time) * 10);
+//        }
+        int result =(int) ((new BigDecimal(time).setScale(1, ROUND_HALF_UP).doubleValue()) * 10);
+        log.info("RESULT: " + result);
+        return result;
+
+//    .    Double truncatedDouble=new BigDecimal(toBeTruncated ).setScale(1, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+
+//    You throw a ball vertically upwards with an initial speed v (in km per hour).
+//    The height h of the ball at each time t is given by h = v*t - 0.5*g*t*t where g is Earth's gravity (g ~ 9.81 m/s**2).
+//    A device is recording at every tenth of second the height of the ball.
+//    For example with v = 15 km/h the device gets something of the following form:
+//    (0, 0.0), (1, 0.367...), (2, 0.637...), (3, 0.808...), (4, 0.881..) ...
+//    where the first number is the time in tenth of second and the second number the height in meter.
+//    Task
+//
+//    Write a function max_ball with parameter v (in km per hour)
+//    that returns the time in tenth of second of the maximum height recorded by the device.
+//
+//    Examples:
+//    max_ball(15) should return 4
+//    max_ball(25) should return 7
+//
+//    Notes:
+//
+//    Remember to convert the velocity from km/h to m/s or from m/s in km/h when necessary.
+//    The maximum height recorded by the device is not necessarily the maximum height reached by the ball.
+
 
 }
--- src/test/java/BallTester.java
+++ src/test/java/BallTester.java
@@ -4,6 +4,7 @@ import main.java.Ball;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class BallTester {
 
@@ -11,11 +12,27 @@ public class BallTester {
     private static void testing(int actual, int expected) {
         assertEquals(expected, actual);
     }
+
+    private static void testing2(int actual, int expected) {
+        assertNotEquals(expected, actual);
+    }
+
     @Test
     public void test() {
         System.out.println("Fixed Tests maxBall");
+        testing(Ball.maxBall(15), 4);
         testing(Ball.maxBall(37), 10);
         testing(Ball.maxBall(45), 13);
         testing(Ball.maxBall(99), 28);
         testing(Ball.maxBall(85), 24);
-    }}
+    }
+
+//    @Test
+//    public void testBoundary(){
+//        testing2(Ball.maxBall(Integer.MAX_VALUE), 10);
+//        testing2(Ball.maxBall(Integer.MIN_VALUE), 10);
+//        testing2(Ball.maxBall(0), 0);
+//        testing2(Ball.maxBall(10), 0);
+//        testing2(Ball.maxBall(0), 10);
+//    }
+}
